80cc5dd86b75069a018732e842e6786993b6871f
Marcos-Tecnologia <marcosagranjareges@gmail.com>
2026-03-16 14:57:08 -0300

    Mudança de  API Capybara para OpenAI

--- api/chat.js
+++ api/chat.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        model: "nousresearch/nous-capybara-7b",
+        model: "openai/gpt-4o",",
         messages
       })
     });
